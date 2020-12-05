6a3e3fc4fa654d8a86c75220d8ccd9459664792a
Tomantou <ptrantis2@yahoo.fr>
2020-12-05 21:56:49 +0100

    updated env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: false,
-  boutiqueBackend: 'https://boutique-back-end.azurewebsites.net/'
+  boutiqueBackend: 'https://boutique-back-end.azurewebsites.net/',
+  boutiqueContainer: "https://epibfilemanager.blob.core.windows.net/boutique/boutique/",
+  authentication_api_url: "https://epib-oauth.azurewebsites.net/",
+  client_id: "client_id",
+  client_secret: "client_secret",
+  scope: "boutique"
 };
