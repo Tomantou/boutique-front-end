6b14eccbf95fd1ae0b5b5abf96c0115de9ab00ba
Tomantou <ptrantis2@yahoo.fr>
2020-12-06 21:40:34 +0100

    trying fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  boutiqueBackend: 'http://[::1]:3000',
+  // boutiqueBackend: 'http://[::1]:3000',
+  boutiqueBackend: 'https://boutique-back-end.azurewebsites.net/',
+
   boutiqueContainer: "https://epibfilemanager.blob.core.windows.net/boutique/boutique/",
   authentication_api_url: "https://epib-oauth.azurewebsites.net/",
   client_id: "client_id",
